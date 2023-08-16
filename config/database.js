fe82821d5c958232719d1b5fe9286b30eafbf36b
Sravesh Nandan <kumarsravesh39@gmail.com>
2023-08-16 09:36:39 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -8,12 +8,9 @@ const conParams = {
 // This is the main function to connect to the database 
 const connecToDb = () => {
     try {
-        mongoose.connect(process.env.DB_URL, conParams).then((con) => {
-            console.log(`Connected to the Database: ${con.connection.host}`)
-        }
-        ).catch((err) => {
-            console.log(`Unable to connect to the database. ${err}`)
-        })
+        mongoose.connect(process.env.DB_URL, conParams)
+            console.log(`Connected to the Database:`)
+        
 
     } catch (error) {
         console.log(`Unable to connect to the database. ${error}`)
